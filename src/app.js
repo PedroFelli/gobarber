bbc9331aa0febc8bc2ee3e6d56f340391a037af8
PedroFelli <pedrofellipeunai@gmail.com>
2019-10-22 10:39:26 -0300

    email de cancelamento

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 import './database';
@@ -6,12 +7,16 @@ import './database';
 class App {
   constructor() {
     this.server = express();
-    this.middlwares();
+    this.middlewares();
     this.routes();
   }
 
-  middlwares() {
+  middlewares() {
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'temp', 'uploads'))
+    );
   }
 
   routes() {
